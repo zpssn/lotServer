61e8fc3cc5c3f989579c43875d24ebfb1ff214c1
zpssn <920167280@qq.com>
2020-04-19 10:02:18 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -70,7 +70,7 @@ function Install()
   [ -z "$Eth" ] && Uninstall "Error! Not found a valid ether. "
   Mac=$(cat /sys/class/net/${Eth}/address)
   [ -z "$Mac" ] && Uninstall "Error! Not found mac code. "
-  URLKernel='https://github.com/LotIndex/lotServer/raw/master/lotServer.log'
+  URLKernel='https://github.com/zpssn/lotServer/raw/master/lotServer.log'
   AcceData=$(wget --no-check-certificate -qO- "$URLKernel")
   AcceVer=$(echo "$AcceData" |grep "$KNA/" |grep "/x$KNB/" |grep "/$KNK/" |awk -F'/' '{print $NF}' |sort -nk 2 -t '_' |tail -n1)
   MyKernel=$(echo "$AcceData" |grep "$KNA/" |grep "/x$KNB/" |grep "/$KNK/" |grep "$AcceVer" |tail -n1)
@@ -83,10 +83,10 @@ function Install()
   AcceBin="acce-"$KNV"-["$KNA"_"$KNN"_"$KNK"]"
   mkdir -p "${AcceTmp}/bin/"
   mkdir -p "${AcceTmp}/etc/"
-  wget --no-check-certificate -qO "${AcceTmp}/bin/${AcceBin}" "https://github.com/LotIndex/lotServer/raw/master/${MyKernel}"
+  wget --no-check-certificate -qO "${AcceTmp}/bin/${AcceBin}" "https://github.com/zpssn/lotServer/raw/master/${MyKernel}"
   [ ! -f "${AcceTmp}/bin/${AcceBin}" ] && Uninstall "Download Error! Not Found ${AcceBin}. "
   Welcome;
-  wget --no-check-certificate -qO "/tmp/lotServer.tar" "https://github.com/LotIndex/lotServer/raw/master/lotServer.tar"
+  wget --no-check-certificate -qO "/tmp/lotServer.tar" "https://github.com/zpssn/lotServer/raw/master/lotServer.tar"
   tar -xvf "/tmp/lotServer.tar" -C /tmp
   acce_ver=$(acce_check ${KNV})
   wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://2333333.azurewebsites.net/keygen.php?ver=${acce_ver}&mac=${Mac}"
